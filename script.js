00c6300cecc8c76281d8bbef8c5fb69964ac2c8d
akaushik456 <kaushikaman239@gmail.com>
2022-10-08 19:49:20 -0700

    add-some-js

--- script.js
+++ script.js
@@ -20,18 +20,48 @@ $('.dropdown').on('click',function(){
   });
 
 
-  var name,email,subject,message;
+  var fullName,email,subject,message;
 
-  function submitForm(event) {
+function submitForm(event) {
     event.preventDefault();
-    console.log(`name: ${name}, email: ${email}`)
+    console.log(`name: ${fullName}, email: ${email}, subject: ${subject}`)
   }
 
  function myFunction(event) {
-  if(event.target.name == 'name') {
-    name = event.target.value
+  if(event.target.name == 'fullName') {
+    fullName = event.target.value
   }
   if(event.target.name == 'email') {
     email = event.target.value
-  }; 
+  }
+  if(event.target.name == 'subject') {
+    subject = event.target.value
+  }
  }
+
+ //sticky navigatoin
+ var num = 100;
+$(window).bind('scroll',function() {
+  if($(window).scrollTop() > num) {
+    $('header').addClass('fixed');
+  } else {
+    $('header').removeClass('fixed');
+  }
+});
+
+//scroll btn
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
